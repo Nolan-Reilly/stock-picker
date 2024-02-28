9fc0c1510ae8963d099ad2d52fb7c0f150d5c8d5
Nolan Reilly <nolanreilly02@gmail.com>
2024-02-27 18:04:58 -0600

    Add stock picker markup

---
+++ stock-picker.rb
@@ -0,0 +1,16 @@
+# Unfinished
+
+stock_prices = [17,3,6,9,15,8,6,1,10]
+
+def stock_picker(prices)
+    # Create a Hash that stores the price as a value, and the indices of the
+    # days. Keys can just be some number
+    buy_sell = {}
+
+    prices.each_with_index do |buy_price, buy_index|
+        prices.each do |sell_price, sell_index|
+            
+        end
+    end
+
+end
